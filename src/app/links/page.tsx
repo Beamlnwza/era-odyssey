32afe19492a766aacbce2997d45820e5fa9d0d1b
Beamlnwza <Supawit04591@pcccr.ac.th>
2023-06-11 11:02:12 +0700

    landing page added

---
+++ src/app/links/page.tsx
@@ -0,0 +1,9 @@
+const LinksPage = () => {
+  return (
+    <div className="flex h-screen w-screen justify-center items-center text-6xl">
+      ไม่มีกร้ากกกกกกกกกกกกกกๆๆๆๆๆกกๆกๆกกกกกกกกก
+    </div>
+  )
+}
+
+export default LinksPage
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,32 @@
+import Link from 'next/link'
+import './page.css'
+
 export default function Home() {
-  return <div>test</div>
+  return (
+    <>
+      <div className="flex w-screen justify-center items-center bg-black text-black flex-col gap-2 animate-rainbow">
+        <div className=" flex h-screen flex-col justify-center items-center">
+          <h1 className="text-5xl font-bold animate-rainbow">
+            ERA ODYSSEY PROJECT
+          </h1>
+          <div className="animate-spin text-lg">IDLE SUPER COOL GAME</div>
+          <div className="flex flex-col justify-center items-center text-2xl gap-2">
+            <h2>สุดยอดเกม idle โครตโหดโครตอันตราย</h2>
+            <h2>
+              ระบบยอดเยี่ยมดี สุดยอดระบบ กาชา microtransactions แดกเงินมึง
+            </h2>
+            <h2>กระโดดๆ ยิงๆๆๆ</h2>
+          </div>
+          <p className="font-bold text-xl text-red-600 animate-pulse">
+            จัดจำหน่าย วันไหนไม่รู้
+          </p>
+          <Link href="/links">
+            <button className="border bg-white flex justify-center items-center h-12 px-2 font-extrabold text-2xl pt-2">
+              DISCORD
+            </button>
+          </Link>
+        </div>
+      </div>
+    </>
+  )
 }
